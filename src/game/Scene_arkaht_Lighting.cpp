7a14fdfbbfbdd175867e134fcfe07b53402bed30
Arthur Cathelain <114919245+arkaht@users.noreply.github.com>
2023-10-31 14:54:15 +0100

    [added] depth test & culling

--- src/game/Scene_arkaht_Lighting.cpp
+++ src/game/Scene_arkaht_Lighting.cpp
@@ -9,77 +9,85 @@
 
 void Scene_arkaht_Lighting::load()
 {
-    //  load shader
-    shader = Assets::loadShader(
-        "assets/shaders/arkaht_Lighting.vert", 
-        "assets/shaders/arkaht_Lighting.frag", 
-        "", "", "", 
-        "arkaht_Lighting"
-    );
-    assert( shader.isValid() );
-    
-    //  load mesh
-    mesh = new CubeMesh();
-    mesh->load();
+	//  load shader
+	shader = Assets::loadShader(
+		"assets/shaders/arkaht_Lighting.vert",
+		"assets/shaders/arkaht_Lighting.frag",
+		"", "", "",
+		"arkaht_Lighting"
+	);
+	assert( shader.isValid() );
 
-    //  create cubes
-    for ( int i = 0; i < CUBES_COUNT; i++ )
-    {
-        cubes.emplace_back( 0.0f, 0.0f, mesh );
-    }
+	//  load mesh
+	mesh = new CubeMesh();
+	mesh->load();
 
-    //  setup projection
-    projection = Matrix4::createPerspectiveFOV( 
-        70.0f, 
-        game->windowWidth, 
-        game->windowHeight, 
-        0.1f, 
-        1000.0f 
-    );
+	//  create cubes
+	for ( int i = 0; i < CUBES_COUNT; i++ )
+	{
+		cubes.emplace_back( 0.0f, 0.0f, mesh );
+	}
+
+	//  setup projection
+	projection = Matrix4::createPerspectiveFOV(
+		70.0f,
+		game->windowWidth,
+		game->windowHeight,
+		0.1f,
+		1000.0f
+	);
+
+	//  enable culling
+	glEnable( GL_CULL_FACE );
+	glFrontFace( GL_CW );
+
+	//  enable depth test
+	glEnable( GL_DEPTH_TEST );
+	glDepthFunc( GL_LEQUAL );
 }
 
 void Scene_arkaht_Lighting::clean()
 {
-    //  release mesh
-    mesh->clean();
-    delete mesh;
+	//  release mesh
+	mesh->clean();
+	delete mesh;
 }
 
 void Scene_arkaht_Lighting::update( float dt )
 {
-    const float time = Timer::getTimeSinceStart() * 1.0f;
-    const float angle_step = Maths::toRadians( 360.0f / CUBES_COUNT );
+	const float time = Timer::getTimeSinceStart() * 1.0f;
+	const float angle_step = Maths::toRadians( 360.0f / CUBES_COUNT );
 
-    for ( int i = 0; i < CUBES_COUNT; i++ )
-    {
-        CubeObject& cube = cubes[i];
+	for ( int i = 0; i < CUBES_COUNT; i++ )
+	{
+		CubeObject& cube = cubes[i];
 
-        float radius = 1.5f + Maths::sin( i * 0.2f + time ) * 0.1f;
-        float t = time + i * angle_step;
+		float radius = 1.5f + Maths::sin( i * 0.2f + time ) * 0.1f;
+		float t = time + i * angle_step;
 
-        cube.setPosition(
-            Maths::cos( t ) * radius,
-            Maths::sin( t ) * radius,
-            -5.0f + Maths::cos( i * 1.5f + t ) * 5.0f
-        );
+		cube.setPosition(
+			Maths::cos( t ) * radius,
+			Maths::sin( t ) * radius,
+			-5.0f + Maths::cos( i * 1.5f + t ) * 5.0f
+		);
 
-        cube.setScale( Maths::cos( i + t ) * Maths::sin( i + t ) * 3.0f );
-    }
+		cube.setScale( Maths::cos( i + t ) * Maths::sin( i + t ) * 3.0f );
+	}
 }
 
 void Scene_arkaht_Lighting::draw()
 {
-    //  clear screen
-    static const GLfloat bgColor[] = { 0.0f, 0.0f, 0.2f, 1.0f };
-    glClearBufferfv( GL_COLOR, 0, bgColor );
+	//  clear screen
+	static const GLfloat bgColor[] = { 0.0f, 0.0f, 0.2f, 1.0f };
+	glClearBufferfv( GL_COLOR, 0, bgColor );
 
-    //  send projection matrix
-    shader.use();
-    shader.setMatrix4( "proj_matrix", projection );
+	//  send projection matrix
+	shader.use();
+	shader.setMatrix4( "proj_matrix", projection );
 
-    for ( int i = 0; i < CUBES_COUNT; i++ )
-    {
-        CubeObject& cube = cubes[i];
-        cube.draw( shader );
-    }
+	for ( int i = 0; i < CUBES_COUNT; i++ )
+	{
+		CubeObject& cube = cubes[i];
+		cube.draw( shader );
+	}
 }
--- src/game/Scene_arkaht_Lighting.h
+++ src/game/Scene_arkaht_Lighting.h
@@ -6,31 +6,32 @@
 #include "CubeObject.h"
 #include "CubeMesh.h"
 
-class Scene_arkaht_Lighting : public Scene 
+class Scene_arkaht_Lighting : public Scene
 {
 public:
-    Scene_arkaht_Lighting() {}
-    ~Scene_arkaht_Lighting() { clean(); }
+	Scene_arkaht_Lighting() {}
+	~Scene_arkaht_Lighting() { clean(); }
 
-    void setGame( Game* _game ) { game = _game; }
+	void setGame( Game* _game ) { game = _game; }
 
-    void load();
-    void clean();
+	void load();
+	void clean();
 
-    void pause() {}
-    void resume() {}
-    void handleEvent( const InputState& state ) {}
-    
-    void update( float dt );
-    void draw();
+	void pause() {}
+	void resume() {}
+	void handleEvent( const InputState& state ) {}
+
+	void update( float dt );
+	void draw();
 
 private:
-    Game* game { nullptr };
-    Shader shader;
+	Game* game { nullptr };
+	Shader shader;
 
-    CubeMesh* mesh { nullptr };
-    const int CUBES_COUNT = 256;
-    std::vector<CubeObject> cubes;
+	CubeMesh* mesh { nullptr };
+	const int CUBES_COUNT = 256;
+	std::vector<CubeObject> cubes;
 
-    Matrix4 projection;
+
+	Matrix4 projection;
 };
